eaf145ccb75b0514db2ddc76a7584a0a54f0e82f
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2020-12-10 20:00:28 +0600

    second push

--- views/editStudent.php
+++ views/editStudent.php
@@ -7,16 +7,16 @@
 
 </head>
 <body>
-<form name="" action="" method="post" onsubmit="return userRegistrationValidate()">
+<form name="editValidation" action="" method="post" onsubmit="return editStudent()">
     <table align="center">
         <tr>
             <td align="right">Student Id:</td>
-            <td><input id="name" type="text" id="studentId" name="studentId"> <span id="err_studentId"></span>
+            <td><input type="text" id="studentId" name="studentId"> <span id="err_studentId"></span>
 
         </tr>
         <tr>
             <td align="right">Name:</td>
-            <td><input id="name" type="text" id="name" name="name"> <span id="err_name"></span>
+            <td><input type="text" id="sname" name="sname"> <span id="err_sname"></span>
                 <script>
 
                 </script>
@@ -26,30 +26,55 @@
             </td>
         </tr>
         <tr>
-            <td align="right">
-                DOB:</td>
-            <td><input type="text" id="dob" name="dob"><span id="err_dob"></span>
-                <?php
-                //echo $err_username;
-                ?>
+            <td align="right">DOB:</td>
+            <!--
+            <td>
+                <input type="date" name="dob" id="dob"> <span id="err_dob"></span>
+            </td>
+            -->
+            <td>
+                <select name="day" id="day">
+                    <option disabled selected> Day </option>"
+                    <?php
+
+                    for($i = 1; $i < 32; $i++) {
+                        echo "<option value=" . $i . ">" . $i . "</option>";
+                    }
+                    ?>
+                </select>
+                <select name="month" id="month">
+                    <option disabled selected> Month </option>"
+                    <?php
+                    $month = array("Jan", "Feb","March","April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec");
+                    for($i = 0; $i <= 11; $i++){
+                        echo "<option value=" . $month[$i] . ">" . $month[$i] . "</option>";
+                    }
+                    ?>
+
+                </select>
+                <select name="year" id="year">
+                    <option disabled selected> Year </option>"
+                    <?php
+                    for($i = 1997; $i <= 2007; $i++){
+                        echo "<option value=" . $i . ">" . $i . "</option>";
+                    }
+                    ?>
+                </select>
             </td>
         </tr>
         <tr>
             <td align="right">Credit:</td>
-            <td><input type="text" id="credit" name="credit"><span id="err_credit"></span>
-                <?php
-                //echo $err_pass;
-                ?>
+            <td>
+                <input type="text" id="credit" name="credit"><span id="err_credit"></span>
             </td>
         </tr>
 
         <tr>
             <td align="right">
                 Cgpa:</td>
-            <td><input type="text" id="cgpa" name="cgpa"><span id="err_cgpa"></span>
-                <?php
-                //echo $err_email;
-                ?>
+
+            <td>
+                <input type="text" id="cgpa" name="cgpa"><span id="err_cgpa"></span>
             </td>
         </tr>
         <tr>
@@ -68,6 +93,6 @@
         </tr>
     </table>
 </form>
-<script src="../controllers/userValidate.js"></script>
+<script src="js/editValidate.js"></script>
 </body>
 </html>
---
+++ views/js/editValidate.js
@@ -0,0 +1,153 @@
+
+function editStudent(){
+    //alert("hello ");
+    refresh();
+    var has_err=false;
+    /*var username=getElement("username");
+    var password=getElement("pass");
+    var err_username=getElement("err_username");
+    var err_password=getElement("err_pass");
+
+    var username = document.forms["loginForm"]["username"].value;
+    //alert(username);
+    var password = document.forms["loginForm"]["pass"].value;*/
+
+    var e_sname = getElement("sname");
+    var e_studentId = getElement("studentId");
+    /*var e_day = getElement("day")
+    var e_month = getElement("month");
+    var e_year = getElement("year");*/
+    var e_credit = getElement("credit")
+    var e_cgpa = getElement("cgpa");
+    var e_deptId = getElement("deptId");
+
+    var err_sname = getElement("err_sname");
+    var err_studentId = getElement("err_studentId");
+    /*var err_day = getElement("err_day");
+    var err_month = getElement("err_month");
+    var err_year = getElement("err_year");*/
+    var err_credit = getElement("err_credit");
+    var err_cgpa = getElement("err_cgpa");
+    var err_deptId = getElement("err_deptId");
+
+    var hasErr = false;
+
+    if(e_studentId.value == ""){
+        hasErr= true;
+        err_studentId.innerHTML = "ID Required";
+        e_studentId.focus();
+        return !hasErr;
+    }
+    if(e_sname.value == ""){
+        hasErr= true;
+        err_sname.innerHTML = "Name Required";
+        e_sname.focus();
+        return !hasErr;
+    }
+    /*if(e_day.value == ""){
+        hasErr= true;
+        err_day.innerHTML = "DOB Required";
+        e_day.focus();
+        return !hasErr;
+    }*/
+    if(e_credit.value == ""){
+        hasErr= true;
+        err_credit.innerHTML = "Credit Required";
+        e_credit.focus();
+        return !hasErr;
+    }
+    if(e_cgpa.value == ""){
+        hasErr= true;
+        err_cgpa.innerHTML = "CGPA Required";
+        e_cgpa.focus();
+        return !hasErr;
+    }
+    if(e_deptId.value == ""){
+        hasErr= true;
+        err_deptId.innerHTML = "Department ID Required";
+        e_deptId.focus();
+        return !hasErr;
+    }
+
+    return !hasErr;
+}
+
+function refresh(){
+    /*var err_sname = getElement("err_sname");
+    var err_password = getElement("err_pass");*/
+    //var err_username = getElement("err_username");
+
+    var err_sname = getElement("err_sname");
+    var err_studentId = getElement("err_studentId");
+    /*var err_day = getElement("err_day");
+    var err_month = getElement("err_month");
+    var err_year = getElement("err_year");*/
+    var err_credit = getElement("err_credit");
+    var err_cgpa = getElement("err_cgpa");
+    var err_deptId = getElement("err_deptId");
+
+    /*err_username.innerHTML = "";
+    err_pass.innerHTML = "";*/
+    //err_username.innerHTML = "";
+
+    err_sname.innerHTML = "";
+    err_studentId.innerHTML = "";
+    /*err_day.innerHTML = "";
+    err_month.innerHTML = "";
+    err_year.innerHTML = "";*/
+    err_credit.innerHTML = "";
+    err_cgpa.innerHTML = "";
+    err_deptId.innerHTML = "";
+
+}
+
+function getElement(id){
+    return document.getElementById(id);
+}
+
+/*function studentValidate(){
+    //alert("hello ");
+    refresh1();
+    var has_err=false;*/
+    /*var username=getElement("username");
+    var password=getElement("pass");
+    var err_username=getElement("err_username");
+    var err_password=getElement("err_pass");
+
+    var username = document.forms["loginForm"]["username"].value;
+    //alert(username);
+    var password = document.forms["loginForm"]["pass"].value;*/
+
+    /*var e_name = getElement("sname");
+    var e_studentId = getElement("studentId");
+    var err_name = getElement("err_sname");
+    var err_studentId = getElement("err_studentId");
+
+    var hasErr = false;
+
+    if(e_name.value == ""){
+        hasErr= true;
+        err_name.innerHTML = "Username Required";
+        e_name.focus();
+        return !hasErr;
+    }
+    if(e_studentId.value == ""){
+        hasErr= true;
+        err_studentId.innerHTML = "Password Required";
+        e_studentId.focus();
+        return !hasErr;
+    }
+
+    return !hasErr;
+}*/
+
+/*function refresh1(){
+    var err_sname = getElement("err_sname");
+    var err_studentId = getElement("err_studentId");
+    //var err_username = getElement("err_username");
+
+    err_sname.innerHTML = "";
+    err_studentId.innerHTML = "";
+    //err_username.innerHTML = "";
+
+}*/
